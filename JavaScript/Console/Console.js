4bbc09e412082474e38d93d97f5fc34eac98b384
cmv <68516521+cm-verbose@users.noreply.github.com>
2022-04-17 21:01:41 -0400

    Create Console.js

---
+++ JavaScript/Console/Console.js
@@ -0,0 +1,25 @@
+/* 
+
+console.assert() 
+
+*/
+console.assert(1 === 2) 
+// Assertion failed
+console.assert(1 > 0.5)
+// undefined
+
+/* 
+
+console.clear() 
+
+*/
+console.clear()
+
+
+
+/* 
+
+console.context()
+
+*/
+console.log(console); 
