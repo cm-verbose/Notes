6446f087d72f069252c04bba6fc3321965ddd8a8
cmv <68516521+cm-verbose@users.noreply.github.com>
2022-04-21 21:37:22 -0400

    Update Console.js

--- JavaScript/Console/Console.js
+++ JavaScript/Console/Console.js
@@ -1,39 +1,11 @@
-/* Examples */
-
-
-/* 
-
-console.assert() 
-
-*/
-
-
-
+/* console.assert() */
 console.assert(1 === 2) 
 // Assertion failed
 console.assert(1 > 0.5)
 // undefined
 
-
-
-/* 
-
-console.clear() 
-
-*/
-
-
-
+/* console.clear() */
 console.clear()
 
-
-
-/* 
-
-console.context()
-
-*/
-
-
-
+/* console.context()*/
 console.log(console); 
