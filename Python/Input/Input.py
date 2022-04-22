f6c470ef46d19106b34c3da441778799fa9bb9cb
cmv <68516521+cm-verbose@users.noreply.github.com>
2022-04-22 13:15:52 -0400

    Update Input.py

--- Python/Input/Input.py
+++ Python/Input/Input.py
@@ -8,4 +8,5 @@ print("Hi " + name + " !") # if you had entered a name here, it will print out "
 
 name = input("What is your name ? \n")
 age = input("What is your age ? \n")
-print("Hi " + name + " you are " + age + " years old") # Returns both the age and name, for example if name = "John" and age = "22", it will return "Hi John you are 22 years old"
+print("Hi " + name + " you are " + age + " years old") 
+# Returns both the age and name, for example if name = "John" and age = "22", it will return "Hi John you are 22 years old"
