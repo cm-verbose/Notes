be3b77dfc14425074f36a8252ca46a38f87d1b65
cmv <68516521+cm-verbose@users.noreply.github.com>
2022-04-21 21:36:43 -0400

    Create Input.py

---
+++ Python/Input/Input.py
@@ -0,0 +1,11 @@
+# Single inputs
+
+name = input("What is your name ? \n") # here \n is a newline, so it prints to the next line
+print("Hi " + name + " !") # if you had entered a name here, it will print out "Hi John", if you would have typed "John"
+
+
+# Using multiple inputs
+
+name = input("What is your name ? \n")
+age = input("What is your age ? \n")
+print("Hi " + name + " you are " + age + " years old") # Returns both the age and name, for example if name = "John" and age = "22", it will return "Hi John you are 22 years old"
