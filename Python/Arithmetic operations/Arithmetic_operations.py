d56602740ad174977337fcfba99b752f655aefb4
cmv <68516521+cm-verbose@users.noreply.github.com>
2022-04-24 13:10:45 -0400

    Create Arithmetic_operations.py

---
+++ Python/Arithmetic operations/Arithmetic_operations.py
@@ -0,0 +1,66 @@
+# Addition
+ans = 10 + 9 
+print(ans) # 19 
+
+# Substraction
+ans = 9 - 10
+print(ans) # -1 
+
+# Multiplication
+ans = 9 * 10
+print(ans) # 90
+
+# Division
+ans = 8/9
+print(ans) # 0.8888888888888888
+
+# Floor division
+ans = 30 // 9 
+print(ans) # 3
+
+# Modulo 
+ans = 10 % 5
+print(ans) # 0 
+ans = 10 % 6
+print(ans) # 4
+
+# Exponentiation
+ans = 5 ** 5
+print(ans) # 3125
+ans = 81 ** 0.5
+print(ans) # 9.0
+
+# Augmented addition operator
+ans = 5; 
+ans += 14
+print(ans)  # 19
+
+# Augmented substraction operator
+ans = 5; 
+ans -= 14
+print(ans)  # -9 
+
+# Augmented multiplication operator
+ans = 5
+ans *= 14
+print(ans) # 70
+
+# Augmented division operator
+ans = 5
+ans /= 14
+print(ans) # 0.35714285714285715
+
+# Augmented floor division operator
+ans = 5
+ans //= 14
+print(ans) # 0
+
+# Augmented modulo operator
+ans = 5
+ans %= 14
+print(ans) # 5
+
+# Augmented exponentiation operator
+ans = 5
+ans **= 14
+print(ans) # 6103515625
