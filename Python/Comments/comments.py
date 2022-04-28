7b9b776e6d5f8a028f18643a95bbe4163fb54eec
cmv <68516521+cm-verbose@users.noreply.github.com>
2022-04-27 22:31:53 -0400

    Create comments.py

---
+++ Python/Comments/comments.py
@@ -0,0 +1,21 @@
+# SINGLE LINE COMMENTS
+
+# This is a comment 
+print("This is valid Python")
+
+# MULTI-LINE COMMENTS
+"""This
+   is a 
+   multi
+   line
+   comment
+"""
+
+'''This
+   is a 
+   multi
+   line
+   comment
+    too
+'''
+print("This is valid Python")
